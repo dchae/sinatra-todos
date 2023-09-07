d3210144c95edb9905f297f8e99126132df5f862
dchae <daniel.j.chae@gmail.com>
2023-09-06 23:17:32 -0700

    added list name validation and error flash msgs

--- todo.rb
+++ todo.rb
@@ -25,18 +25,31 @@ get '/lists/new' do
   erb :new_list, layout: :layout
 end
 
-# Create a new todo list
-post '/lists' do
-  session[:lists] << { name: params[:list_name], todos: [] }
-  session[:success] = "The list has been created."
-  redirect '/lists'
+def error_for_list_name(list_name)
+  if !(1..100).include?(list_name.size)
+    'List name must be between 1 and 100 characters.'
+  elsif session[:lists].any? { |list| list[:name] == list_name }
+    'List name must be unique.'
+  end
 end
 
+# Create a new todo list
+post '/lists' do
+  list_name = params[:list_name].strip
+  error = error_for_list_name(list_name)
+
+  if error
+    session[:error] = error
+    erb :new_list, layout: :layout
+  else
+    session[:lists] << { name: list_name, todos: [] }
+    session[:success] = 'The list has been created.'
+    redirect '/lists'
+  end
+end
 
 get '/lists/:list_id' do |list_id|
-
 end
 
 get '/lists/:list_id/todos/todo_id' do |list_id, todo_id|
-
 end
